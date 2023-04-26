a77701f871113659847eca44b4e5aa13086a16ed
User <User@DESKTOP-I0JULNS>
2023-04-26 16:20:42 -0300

    Criação de Projeto

---
+++ primeiro_programa_java/src/primeiro_programa_java/primeiraClasseJava.java
@@ -0,0 +1,15 @@
+ package primeiro_programa_java;
+
+ public class primeiraClasseJava {
+	
+	 /*main CTRL+Espaço =preenchimento automatico da linha*/
+   public static void main(String[] args) {
+	
+		  /*sys CTRL+Espaço = preenchimento automatico*/
+        System.out.println("Meu primeiro programa em java");
+}
+	
+	
+
+} 	 
+   
