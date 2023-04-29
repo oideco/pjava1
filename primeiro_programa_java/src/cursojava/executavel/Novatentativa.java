901b63ae83ad9099ad02d865994af55dd97ead68
User <User@DESKTOP-I0JULNS>
2023-04-29 11:43:16 -0300

    Atualização nome e sobrenome

---
+++ primeiro_programa_java/src/cursojava/executavel/Novatentativa.java
@@ -0,0 +1,20 @@
+package cursojava.executavel;
+
+public class Novatentativa {
+
+	public static void main(String[] args) {
+	 
+		 /*Preciso muito fazer isso funcionar*/
+		 System.out.println("Meu primeiro sucesso na programação");
+		 
+		 System.out.println("Espero muito que de certo");
+			
+	
+	}
+		
+	}
+		
+	
+
+
+
