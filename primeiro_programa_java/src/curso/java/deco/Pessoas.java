1126d35f34b278bed2bb2a69ca6a7e0d438e2ec3
User <User@DESKTOP-I0JULNS>
2023-04-29 12:27:13 -0300

    mew

--- primeiro_programa_java/src/curso/java/deco/Pessoas.java
+++ primeiro_programa_java/src/curso/java/deco/Pessoas.java
@@ -1,5 +1,32 @@
 package curso.java.deco;
 
 public class Pessoas {
+	
+	private String Nome;
+	
+	private String Sobrenome;
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getSobrenome() {
+		return Sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		Sobrenome = sobrenome;
+	}
+	 
+	 
+	
 
 }
+
+
+
+
