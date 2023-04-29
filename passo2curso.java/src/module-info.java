f1af7bfa36e020bdee8a4c6dea4d1680f4991a95
User <User@DESKTOP-I0JULNS>
2023-04-29 11:53:50 -0300

    Atualização Nome e Sobrenome

---
+++ passo2curso.java/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author User
+ *
+ */
+module passo2curso.java {
+}
---
+++ passo2curso.java/src/passo2curso/java/Pessoas.java
@@ -0,0 +1,31 @@
+package passo2curso.java;
+
+public class Pessoas {
+
+	
+	private String Nome;
+	
+	private String Sobrenome;
+
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getSobrenome() {
+		return Sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		Sobrenome = sobrenome;
+	}
+	
+	
+	
+	
+	
+}
